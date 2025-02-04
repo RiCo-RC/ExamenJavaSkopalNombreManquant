4c41c894d9c63172f1696884561492c2538a160f
Rico <rieffel.christopher.dev@gmail.com>
2025-02-04 08:16:37 +0100

    Creating the function and displaying the result

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static int findMissingNumber(int[] numbers) {
+        Arrays.sort(numbers);
+        int missingNumber = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            int j = i + 1;
+            if (numbers[i] != j) {
+                missingNumber = j;
+                break;
+            }
+        }
+        return missingNumber;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 4, 5, 6};
+        int[] numbers2 = {2, 3, 1, 5};
+        System.out.println("La liste des nombres : " + Arrays.toString(numbers));
+        System.out.println("Le nombre manquant est : " + findMissingNumber(numbers));
+        System.out.print("\n");
+        System.out.println("La liste des nombres : " + Arrays.toString(numbers2));
+        System.out.println("Le nombre manquant est : " + findMissingNumber(numbers2));
+    }
+}
